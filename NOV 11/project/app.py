19f79accc922aa4fff706313f699c370e82c46f6
Nandhini L <86370900+Nandhini0720@users.noreply.github.com>
2022-11-11 00:42:51 +0530

    Add files via upload

---
+++ NOV 11/project/app.py
@@ -0,0 +1,185 @@
+from flask import Flask,render_template,request,url_for,flash,redirect,session
+import ibm_db
+import sendgrid
+import os
+import re
+from sendgrid.helpers.mail import *
+
+
+app = Flask(__name__)
+app.secret_key="1"
+
+conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=824dfd4d-99de-440d-9991-629c01b3832d.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=30119;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=nyb16417;PWD=BdnbqnFcLHDyszAv",'','')
+
+@app.route("/")
+def index():
+    return render_template('home.html')
+
+@app.route("/home")
+def home_page():
+    return render_template('home.html')
+
+@app.route("/success")
+def success():
+    return render_template('success.html')
+#------------------------------------------------------
+
+#---------------------------------------------------------
+
+@app.route("/login",methods = ['POST', 'GET'])
+def login():
+    global userid
+    msg = ''
+    if request.method == 'POST' :
+        username = request.form['username']
+        password = request.form['password']
+        sql = "SELECT * FROM LOGIN WHERE username =? AND password=?"
+        stmt = ibm_db.prepare(conn, sql)
+        ibm_db.bind_param(stmt,1,username)
+        ibm_db.bind_param(stmt,2,password)
+        ibm_db.execute(stmt)
+        account = ibm_db.fetch_assoc(stmt)
+        print (account)
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['USERNAME']
+            userid=  account['USERNAME']
+            session['username'] = account['USERNAME']
+            msg = 'Logged in successfully !'
+            
+            return render_template('user_profile.html', msg = msg)
+        else:
+            msg = 'Incorrect username / password !'
+    return render_template('login.html', msg = msg)
+
+#---------------------------------------------------------
+# After login
+@app.route('/afterlogin')
+def afterlogin():
+    return render_template("user_profile.html")
+
+#-------------------------------------------------------
+
+@app.route("/signin",methods = ['POST', 'GET'])
+def signin():
+    msg = ''
+    if request.method == 'POST' :
+        username = request.form['username']
+        usermail = request.form['usermail']
+        usercontact = request.form['usercontact']
+        password = request.form['password']
+        sql = "SELECT * FROM LOGIN WHERE username =?"
+        stmt = ibm_db.prepare(conn, sql)
+        ibm_db.bind_param(stmt,1,username)
+        ibm_db.execute(stmt)
+        account = ibm_db.fetch_assoc(stmt)
+        print(account)
+        if account:
+            msg = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', usermail):
+            msg = 'Invalid email address !'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'name must contain only characters and numbers !'
+        else:
+            insert_sql = "INSERT INTO LOGIN VALUES (?, ?, ?, ?)"
+            prep_stmt = ibm_db.prepare(conn, insert_sql)
+            ibm_db.bind_param(prep_stmt, 1, username)
+            ibm_db.bind_param(prep_stmt, 2, usermail)
+            ibm_db.bind_param(prep_stmt, 3, usercontact)
+            ibm_db.bind_param(prep_stmt, 4, password)
+            ibm_db.execute(prep_stmt)
+            msg = 'You have successfully registered !'
+            mailtest(usermail)
+            return render_template('login.html', msg = msg)
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+
+    return render_template('signin.html', msg = msg)
+
+#sendgrid integration
+def mailtest(to_email):
+    sg = sendgrid.SendGridAPIClient(api_key= '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' )
+    from_email = Email("chinnukool72@gmail.com")
+    subject = "Registration Successfull!"
+    content = Content("text/plain", "")
+    mail = Mail(from_email, to_email, subject, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+#-------------------------------------------------------------------------------
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+@app.route("/adddonor",methods = ['POST','GET'])
+def adddonor():
+    if request.method == 'POST':
+        name = request.form['name']
+        mobile = request.form['mobile']
+        email = request.form['email']
+        age = request.form['age']
+        gender = request.form['gender']
+        blood = request.form['blood']
+        crpassword = request.form['crpassword']
+        cnpassword = request.form['cnpassword']
+        area = request.form['area']
+        city = request.form['city']
+        district = request.form['district']
+
+        sql = "SELECT * FROM students WHERE name =?"
+        stmt = ibm_db.prepare(conn, sql)
+        ibm_db.bind_param(stmt,1,name)
+        ibm_db.execute(stmt)
+        account = ibm_db.fetch_assoc(stmt)
+
+        if account:
+            return render_template('donor.html', msg="You are already a member, please login using your details")
+        else:
+            insert_sql = "INSERT INTO DONOR VALUES (?,?,?,?,?,?,?,?,?,?,?)"
+            prep_stmt = ibm_db.prepare(conn, insert_sql)
+            ibm_db.bind_param(prep_stmt, 1, name)
+            ibm_db.bind_param(prep_stmt, 2, mobile)
+            ibm_db.bind_param(prep_stmt, 3, email)
+            ibm_db.bind_param(prep_stmt, 4, age)
+            ibm_db.bind_param(prep_stmt, 5, gender)
+            ibm_db.bind_param(prep_stmt, 6, blood)
+            ibm_db.bind_param(prep_stmt, 7, crpassword)
+            ibm_db.bind_param(prep_stmt, 8, cnpassword)
+            ibm_db.bind_param(prep_stmt, 9, area)
+            ibm_db.bind_param(prep_stmt, 10, city)
+            ibm_db.bind_param(prep_stmt, 11, district)
+            ibm_db.execute(prep_stmt)
+            return render_template('success.html', msg="Registered successfuly..")
+#-----------------------------------------------------------------------------------------
+
+@app.route('/donorlist')
+def donorlist():
+    donor = []
+    sql = "SELECT * FROM DONOR"
+    stmt = ibm_db.exec_immediate(conn, sql)
+    dictionary = ibm_db.fetch_both(stmt)
+    while dictionary != False:
+        donor.append(dictionary)
+        dictionary = ibm_db.fetch_both(stmt)
+    if donor:
+        return render_template("donor.html", donor = donor)
+
+#----------------------------------------------------------------------------
+@app.route("/request_page")
+def request_page():
+    return render_template('request.html')
+
+@app.route("/admin")
+def admin():
+    return render_template('admin_login.html')
+
+#-----------------------------------------
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
